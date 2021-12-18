6963b647576eda95fcbdb7cfcd7cbf58ace9989b
Tim Rager <tim.rager@gmail.com>
2021-12-18 12:24:14 -0800

    completed header compontent with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import './style.css';
 
 const App = () => {
   return (
-    <h1> TEST </h1>
+    <div>
+      <Header />
+    </div>
   )
 }
 
