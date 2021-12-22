0985b43adb4dca32d58eb832b03ec3e61a7dcb87
Tim Rager <tim.rager@gmail.com>
2021-12-22 13:04:53 -0800

    added state

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return(
         <header className="header">
-            <img src="/images/troll-face.png" className="header--image"/>
+            <img src="/images/troll-face.png" className="header--image" />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
--- src/Meme.js
+++ src/Meme.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import memesData from './memesData';
 
 const Meme = () => {
 
+    const [memeImage, setMemeImage] = useState("");
+
+    const getMeme = () => {
+
+        let randomMeme = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)];
+
+        setMemeImage(randomMeme.url)
+
+    }
+
     return (
         <main>
-            <form className="form">
-                <input
+            <div className="form">
+                <input 
                     type="text"
                     placeholder="Top text"
-                    className="form--input"    
-                />
-                <input
-                    type="text"
-                    placeholder="Button text"
                     className="form--input"
                 />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMeme}>
+                    Get a new meme image 🖼
+                </button>
+                <img src={memeImage} className="meme--image" />
+            </div>
         </main>
     )
 
